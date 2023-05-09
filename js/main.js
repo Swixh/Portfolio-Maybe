8ace5d1de1b23312cfd50026bb6d40986e23ec35
Osvaldo Trevino <svldtrvn58@gmail.com>
2023-05-09 06:56:01 -0500

    Applied Dark to Light Toggle Might Need to Fix Later

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
-const hamburger = document.querySelector('#hamburger');
-const mobileNav = document.querySelector('#mobile');
+const eyes = document.querySelectorAll('.eyes');
 
-hamburger.addEventListener('click', () => {
-  mobileNav.classList.toggle('show');
+if (document.documentElement.getAttribute('data-theme') === 'dark') {
+  // If dark theme is selected the show light eye to switch to light
+  // vise versa
+  // get
+  const darkEyes = document.getElementById('dark-eye');
+  darkEyes.classList.toggle('hidden');
+} else {
+  const lightEye = document.getElementById('light-eye');
+  lightEye.classList.toggle('hidden');
+}
+const darkEye = document.getElementById('dark-eye');
+const lightEye = document.getElementById('light-eye');
+eyes.forEach((eye) => {
+  eye.addEventListener('click', () => {
+    // start eye is has no hidden class
+    // so if we click on it it should add the class hidden and remove the eye that has the hidden class already
+    if (eye.id === 'dark-eye') {
+      darkEye.classList.toggle('hidden');
+      lightEye.classList.remove('hidden');
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      lightEye.classList.toggle('hidden');
+      darkEye.classList.remove('hidden');
+      document.documentElement.setAttribute('data-theme', 'light');
+    }
+  });
 });
