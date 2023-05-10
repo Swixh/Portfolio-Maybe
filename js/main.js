37bdecca86e73dcf45069b79842fd94bc120fb14
Osvaldo Trevino <svldtrvn58@gmail.com>
2023-05-09 21:40:52 -0500

    Before Disaster

--- js/main.js
+++ js/main.js
@@ -1,28 +1,24 @@
 const eyes = document.querySelectorAll('.eyes');
+const darkEyes = document.getElementById('dark-eye');
+const lightEyes = document.getElementById('light-eye');
 
 if (document.documentElement.getAttribute('data-theme') === 'dark') {
-  // If dark theme is selected the show light eye to switch to light
-  // vise versa
-  // get
-  const darkEyes = document.getElementById('dark-eye');
   darkEyes.classList.toggle('hidden');
 } else {
-  const lightEye = document.getElementById('light-eye');
-  lightEye.classList.toggle('hidden');
+  lightEyes.classList.toggle('hidden');
 }
-const darkEye = document.getElementById('dark-eye');
-const lightEye = document.getElementById('light-eye');
+
 eyes.forEach((eye) => {
   eye.addEventListener('click', () => {
     // start eye is has no hidden class
     // so if we click on it it should add the class hidden and remove the eye that has the hidden class already
     if (eye.id === 'dark-eye') {
-      darkEye.classList.toggle('hidden');
-      lightEye.classList.remove('hidden');
+      darkEyes.classList.toggle('hidden');
+      lightEyes.classList.remove('hidden');
       document.documentElement.setAttribute('data-theme', 'dark');
     } else {
-      lightEye.classList.toggle('hidden');
-      darkEye.classList.remove('hidden');
+      lightEyes.classList.toggle('hidden');
+      darkEyes.classList.remove('hidden');
       document.documentElement.setAttribute('data-theme', 'light');
     }
   });
